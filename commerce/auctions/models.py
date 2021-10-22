eeb5ca5acfea924db5b178ddda05ee663aef9de4
VKrysko <v0yrk0ko@gmail.com>
2021-10-22 08:33:35 +0300

    new commerce model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,15 +7,43 @@ class User(AbstractUser):
     pass
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
-    image = models.ImageField(upload_to='category/%Y/%m/%d', blank=True)
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='categories')
     
     class Meta:
         ordering = ('name',)
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+        indexes = [
+            models.Index(fields=['slug','id', 'name']),
+            models.Index(fields=['name'], name='name_idx'),
+                  ]
 
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_auc = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        indexes = [
+            models.Index(fields=['slug','id', 'name']),
+            models.Index(fields=['name'], name='name_products_idx'),
+                  ]
+
+    def __str__(self):
+        return self.name
